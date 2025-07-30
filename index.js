7abebfb5c9b1c6fac4f01181a21742d5d2e0f76e
SHussain47 <hussain.syed.6732@gmail.com>
2025-07-29 21:21:18 -0400

    feat: more notes

--- index.js
+++ index.js
@@ -48,3 +48,62 @@ for (const key in dog) {
 }
 
 
+const data = [
+  {
+    showName: "One Piece",
+    mc: "Luffy",
+    age: 17,
+  },
+  {
+    showName: "Attack on Titan",
+    mc: "Eren",
+    age: 19,
+  },
+  {
+    showName: "Kingdom",
+    mc: "Xin",
+    age: 18,
+  },
+];
+
+for (let i = 0; i < data.length; i++) {
+  const obj = data[i];
+  console.log(obj);
+  console.log(obj.showName);
+  console.log(obj.mc);
+}
+
+console.log(JSON.stringify(data)); // turns object into string
+let stringifyData = JSON.stringify(data);
+console.log(JSON.parse(stringifyData)); // takes a string that looks like JSON and turns it into object
+
+// **** Interview Question **** //
+// count and list how many copies of each
+const colours = ["red", "red", "blue", "yellow", "yellow", "yellow"];
+
+// red: 2, blue: 1, yellow:3
+const frequencyCounter = {};
+console.log("frequencyCounter is empty", frequencyCounter);
+
+for (let i = 0; i < colours.length; i++) {
+  const colour = colours[i];
+  //if key exist increment otherwise add to object as key 
+  if (colour in frequencyCounter) {
+    frequencyCounter[colour] += 1;
+  } else {
+    frequencyCounter[colour] = 1;
+  }
+}
+
+console.log(frequencyCounter);
+
+// Methods to know
+console.log(colours.join()); // Turns an array of items into one string
+console.log(colours.join(", "));
+
+const str = "The, quick, fox, escaped";
+console.log(str); // Takes a string and splits it into an array based on a character you choose
+console.log(str.split(","));
+
+
+
